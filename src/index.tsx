5c5853969c01eaeaf0c52bdd1644ac6e5f214777
JIHYUN LEE <jihyun.lee@dreamsecurity.com>
2022-05-17 11:16:59 +0900

    [Provider] store 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,13 @@
 import ReactDOM from "react-dom";
 import App from "./App";
+import store from "./store";
+import { Provider } from "react-redux";
 
 const container = document.getElementById("app");
 
-ReactDOM.render(<App />, container);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  container
+);
