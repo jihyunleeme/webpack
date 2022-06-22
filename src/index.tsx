4d6cb7e0c79bdc8ebc8d447d90335b2b4abdb9a4
jihyunlee.me <jihyunlee.me@gmail.com>
2022-06-22 14:16:35 +0900

    [package] 필수 패키지 설치 및 오류 fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom";
 import App from "./App";
-import store from "./";
+import store from "./store";
 import { Provider } from "react-redux";
 
 const container = document.getElementById("app");
